0266ed993ef1d76d28cd7d36ef7c002fca564b39
thefarrukh <farruxfayzullayev01@gmail.com>
2025-05-13 11:12:43 +0500

    app and models

---
+++ accounts/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from accounts.models import User, Cart, CartItem
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'phone_number', 'first_name', 'last_name', 'is_active', 'is_staff')
+    list_display_links = ('id', 'email', 'phone_number', 'first_name', 'last_name')
+    search_fields = ('email', 'phone_number', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    list_display_links = ('id',)
+    search_fields = ('user',)
+
+    inlines = [CartItemInline]
